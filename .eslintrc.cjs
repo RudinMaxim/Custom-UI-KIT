338c3a798969f9db1c918d15ea1781629997138e
Maxim Rudin <>
2024-05-10 23:19:34 +0500

    rm

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,37 +1,15 @@
-// module.exports = {
-//   root: true,
-//   parser: '@typescript-eslint/parser',
-//   parserOptions: {
-//     ecmaVersion: 2020,
-//     sourceType: 'module',
-//     ecmaFeatures: {
-//       jsx: true,
-//     },
-//   },
-//   settings: {
-//     react: {
-//       version: 'detect',
-//     },
-//   },
-//   env: {
-//     browser: true,
-//     amd: true,
-//     node: true,
-//   },
-//   extends: [
-//     'eslint:recommended',
-//     'plugin:@typescript-eslint/recommended',
-//     'plugin:react/recommended',
-//     'plugin:react-hooks/recommended',
-//     'plugin:jsx-a11y/recommended',
-//     'plugin:storybook/recommended',
-//   ],
-//   overrides: [
-//     {
-//       files: ['**/*.stories.*'],
-//       rules: {
-//         'import/no-anonymous-default-export': 'off',
-//       },
-//     },
-//   ],
-// };
+module.exports = {
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended'],
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['react-refresh'],
+    rules: {
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  }
+  

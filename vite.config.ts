2d2b24ed5c572254f145c6fea93d371b7d3b0159
Maxim Rudin <>
2024-05-11 19:24:33 +0500

    add

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,9 +12,9 @@ export default defineConfig({
   },
   build: {
     lib: {
-      entry: path.resolve(__dirname, './src/components/index.ts'),
+      entry: path.resolve(__dirname, './src/index.ts'),
       name: 'RuKit',
-      fileName: 'react-rukit',
+      fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
       external: ['react', 'react-dom'],
@@ -24,6 +25,8 @@ export default defineConfig({
         },
       },
     },
+    sourcemap: true,
+    emptyOutDir: true,
   },
-  plugins: [react()],
+  plugins: [react(), dts()],
 });
